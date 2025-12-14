e8b15214536e57356fa1a9d596072f00ede0e68f
5thDimension-Sean <ss0486034@gmail.com>
2025-12-13 20:50:51 -0500

    first working version
    Signed-off-by: 5thDimension-Sean <ss0486034@gmail.com>

--- yoloExec.py
+++ yoloExec.py
@@ -1,5 +1,16 @@
 from ultralytics import YOLO
 
+'''
 model = YOLO("yolo11n.pt")
-results = model("image.jpg")
+model.train(data="coco128.yaml",epochs=3,imgsz=640,name="yolo11n-results",device=0)
+model.val(data="coco128.yaml",imgsz=640,device=0)
+results = model("images/train/trafficLight.jpg", save=True, project="runs", name="predict")
 results[0].show()
+print(results[0].save_dir)
+
+model.export(format="onnx", dynamic=True)
+'''
+
+model = YOLO("yolo11n.pt")
+
+results = model("images/train/trafficLight.jpg", save=True, show = True)
